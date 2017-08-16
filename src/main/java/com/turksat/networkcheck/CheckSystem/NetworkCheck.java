e9845beb8c4dec7611a7368fe46522bfce30265b
furkanmumcu <furkanmumcu2@gmail.com>
2017-08-16 10:53:05 +0300

    correction in check method fresh db info

--- src/main/java/com/turksat/networkcheck/CheckSystem/NetworkCheck.java
+++ src/main/java/com/turksat/networkcheck/CheckSystem/NetworkCheck.java
@@ -163,157 +163,159 @@ public class NetworkCheck {
             public void run() {
                 //find sunucu
 
-                String hql = "From Sunucu S where S.sunucuId = :id";
 
-                Configuration configuration = new Configuration();
-                configuration.configure();
+                //System.out.println(sunucu.getKontrolPeriyodu());
+                //System.out.println(sunucu.getSunucuSanalAdi());
+                //System.out.println(sunucu.getProtokol());
+                //System.out.println(sunucu.getAktifPasif());
+                //System.out.println("HTTP" == sunucu.getProtokol()); // false
+                //System.out.println("TCP/IP".equals(sunucu.getProtokol()));
 
-                SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-                Session session = sessionFactory.openSession();
-                session.beginTransaction();
+                while(true) {
+                    String hql = "From Sunucu S where S.sunucuId = :id";
 
-                Query query = session.createQuery(hql);
-                query.setParameter("id", sunucuId);
-                List<Sunucu> sunucuList = query.list();
+                    Configuration configuration = new Configuration();
+                    configuration.configure();
 
-                sunucu = sunucuList.get(0);
-                session.close();
+                    SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+                    Session session = sessionFactory.openSession();
+                    session.beginTransaction();
 
-                System.out.println(sunucu.getKontrolPeriyodu());
-                System.out.println(sunucu.getSunucuSanalAdi());
-                System.out.println(sunucu.getProtokol());
-                System.out.println(sunucu.getAktifPasif());
-                System.out.println("HTTP" == sunucu.getProtokol()); // false
-                System.out.println("TCP/IP".equals(sunucu.getProtokol()));
+                    Query query = session.createQuery(hql);
+                    query.setParameter("id", sunucuId);
+                    List<Sunucu> sunucuList = query.list();
 
-                while(sunucu.getAktifPasif()) {
-                    if("TPC/IP".equals(sunucu.getProtokol())){
-                        System.out.println("GIRDIM");
-                        boolean connCheck=false;
+                    sunucu = sunucuList.get(0);
+                    session.close();
+                    if(sunucu.getAktifPasif()) {
+                        if ("TPC/IP".equals(sunucu.getProtokol())) {
+                            System.out.println("GIRDIM");
+                            boolean connCheck = false;
 
-                        try{
-                            Socket clientSocket = new Socket(sunucu.getSunucuIp(), Integer.parseInt(sunucu.getSunucuPortBilgisi()));
-                            clientSocket .setSoTimeout(5000);
-                            connCheck = clientSocket.isConnected();
-                            System.out.println("Server is up... " + "connCheck: " + connCheck);
+                            try {
+                                Socket clientSocket = new Socket(sunucu.getSunucuIp(), Integer.parseInt(sunucu.getSunucuPortBilgisi()));
+                                clientSocket.setSoTimeout(5000);
+                                connCheck = clientSocket.isConnected();
+                                System.out.println("Server is up... " + "connCheck: " + connCheck);
 
-                            //log olustur
-                            Configuration logConfiguration = new Configuration();
-                            logConfiguration.configure();
+                                //log olustur
+                                Configuration logConfiguration = new Configuration();
+                                logConfiguration.configure();
 
-                            SessionFactory logSessionFactory = new Configuration().configure().buildSessionFactory();
-                            Session logSession = logSessionFactory.openSession();
-                            logSession.beginTransaction();
+                                SessionFactory logSessionFactory = new Configuration().configure().buildSessionFactory();
+                                Session logSession = logSessionFactory.openSession();
+                                logSession.beginTransaction();
 
-                            Log log = new Log();
-                            log.setDurum("server is up");
-                            log.setDate(new Date());
-                            log.setTime(new SimpleDateFormat("HH:mm").format(new Date()));
-                            log.setSunucuId(sunucuId);
-                            log.setSunucu(sunucu);
+                                Log log = new Log();
+                                log.setDurum("server is up");
+                                log.setDate(new Date());
+                                log.setTime(new SimpleDateFormat("HH:mm").format(new Date()));
+                                log.setSunucuId(sunucuId);
+                                log.setSunucu(sunucu);
 
-                            logSession.save(log);
-                            logSession.getTransaction().commit();
-                            logSession.close();
+                                logSession.save(log);
+                                logSession.getTransaction().commit();
+                                logSession.close();
+
+                            } catch (Exception e) {
+                                //errorMsg(e);
+                                Configuration logConfiguration = new Configuration();
+                                logConfiguration.configure();
+
+                                SessionFactory logSessionFactory = new Configuration().configure().buildSessionFactory();
+                                Session logSession = logSessionFactory.openSession();
+                                logSession.beginTransaction();
+
+                                Log log = new Log();
+                                log.setDurum("error: " + errorMsg(e));
+                                log.setDate(new Date());
+                                log.setTime(new SimpleDateFormat("HH:mm").format(new Date()));
+                                log.setSunucuId(sunucuId);
+                                log.setSunucu(sunucu);
+
+                                logSession.save(log);
+                                logSession.getTransaction().commit();
+                                logSession.close();
+                            }
 
                         }
-                        catch (Exception e)
-                        {
-                            //errorMsg(e);
-                            Configuration logConfiguration = new Configuration();
-                            logConfiguration.configure();
 
-                            SessionFactory logSessionFactory = new Configuration().configure().buildSessionFactory();
-                            Session logSession = logSessionFactory.openSession();
-                            logSession.beginTransaction();
+                        if ("JDBC".equals(sunucu.getProtokol())) {
+                            Connection c = null;
+                            try {
+                                Class.forName("org.postgresql.Driver");
 
-                            Log log = new Log();
-                            log.setDurum("error: " + errorMsg(e));
-                            log.setDate(new Date());
-                            log.setTime(new SimpleDateFormat("HH:mm").format(new Date()));
-                            log.setSunucuId(sunucuId);
-                            log.setSunucu(sunucu);
-
-                            logSession.save(log);
-                            logSession.getTransaction().commit();
-                            logSession.close();
-                        }
-
-                    }
-
-                    if("JDBC".equals(sunucu.getProtokol())){
-                        Connection c = null;
-                        try {
-                            Class.forName("org.postgresql.Driver");
-
-                            //c = DriverManager
-                              //      .getConnection("jdbc:postgresql://localhost:5432/fortesting",
+                                //c = DriverManager
+                                //      .getConnection("jdbc:postgresql://localhost:5432/fortesting",
                                 //            "postgres", "123");
 
 
-                            c = DriverManager
-                                    .getConnection("jdbc:postgresql://"+ sunucu.getSunucuIp() + ":" + sunucu.getSunucuPortBilgisi()
-                                            + "/", sunucu.getSunucuKullaniciAdi(),sunucu.getSunucuSifre());
-                            System.out.println("Server is up");
-                            c.close();
+                                c = DriverManager
+                                        .getConnection("jdbc:postgresql://" + sunucu.getSunucuIp() + ":" + sunucu.getSunucuPortBilgisi()
+                                                + "/", sunucu.getSunucuKullaniciAdi(), sunucu.getSunucuSifre());
+                                System.out.println("Server is up");
+                                c.close();
 
-                            logOlustur();
+                                logOlustur();
+                            } catch (Exception e) {
+                                //errorMsg(e);
+                                System.out.println("Server is down");
+                                logOlustur(e);
+                            }
                         }
-                        catch (Exception e) {
-                            //errorMsg(e);
-                            System.out.println("Server is down");
-                            logOlustur(e);
+
+                        if ("HTTP".equals(sunucu.getProtokol())) {
+
+
+                            try {
+                                String url = "http://" + sunucu.getSunucuIp();
+
+                                URL obj = new URL(url);
+                                HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+                                System.out.println("http server is up... response code: " + con.getResponseCode());
+                                logOlustur();
+
+                            } catch (Exception e) {
+                                System.out.println("http server is down");
+                                //errorMsg(e);
+                                logOlustur(e);
+                            }
+
+                        }
+
+                        if ("HTTPS".equals(sunucu.getProtokol())) {
+
+
+                            try {
+                                String url = "https://" + sunucu.getSunucuIp();
+
+                                URL obj = new URL(url);
+                                HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+                                System.out.println("https server is up... response code: " + con.getResponseCode());
+                                logOlustur();
+                            } catch (Exception e) {
+                                System.out.println("https server is down");
+                                errorMsg(e);
+                                logOlustur(e);
+                            }
+
+                        }
+
+                        try {
+                            Thread.sleep(sunucu.getKontrolPeriyodu() * 1000);
+                        } catch (InterruptedException e) {
+                            //e.printStackTrace();
                         }
                     }
+                    else{
+                        System.out.println("NOT AKTIF");
 
-                    if("HTTP".equals(sunucu.getProtokol())){
-
-
-                        try{
-                            String url = "http://"+sunucu.getSunucuIp();
-
-                            URL obj = new URL(url);
-                            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-                            System.out.println("http server is up... response code: " + con.getResponseCode());
-                            logOlustur();
-
+                        try {
+                            Thread.sleep(sunucu.getKontrolPeriyodu() * 1000);
+                        } catch (InterruptedException e) {
+                            //e.printStackTrace();
                         }
-                        catch (Exception e)
-                        {
-                            System.out.println("http server is down");
-                            //errorMsg(e);
-                            logOlustur(e);
-                        }
-
                     }
-
-                    if("HTTPS".equals(sunucu.getProtokol())){
-
-
-                        try{
-                            String url = "https://"+sunucu.getSunucuIp();
-
-                            URL obj = new URL(url);
-                            HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
-                            System.out.println("https server is up... response code: " + con.getResponseCode());
-                            logOlustur();
-                        }
-                        catch (Exception e)
-                        {
-                            System.out.println("https server is down");
-                            errorMsg(e);
-                            logOlustur(e);
-                        }
-
-                    }
-
-
-                    try {
-                        Thread.sleep(sunucu.getKontrolPeriyodu() * 1000);
-                    } catch (InterruptedException e) {
-                        //e.printStackTrace();
-                    }
-
                 }
             }
         }; t.start();
